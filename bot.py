4179281b831b8bea81903ccff59bedb07f155ce2
Nikolay1123770 <131786139+Nikolay1123770@users.noreply.github.com>
2026-01-28 11:55:10 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -540,4 +540,6 @@ def webhook_info():
     return response.json()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
